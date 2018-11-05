55deb37b41258c524f5da86a683db0e661d14780
masato <masato@m-mbp.local>
2018-11-05 13:04:46 +0800

    Initial Commit

---
+++ CandleStickChart_iOS_Charts_Programatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CandleStickChart_iOS_Charts_Programatically
+//
+//  Created by masato on 5/11/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
